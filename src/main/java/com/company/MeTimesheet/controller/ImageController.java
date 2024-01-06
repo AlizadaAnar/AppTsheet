861db70cbb1af43bcc101a9c9909e7f0bba71ea5
anaralizada <anar_alizade@internet.ru>
2024-01-06 14:44:59 +0400

    last changed

--- src/main/java/com/company/MeTimesheet/controller/ImageController.java
+++ src/main/java/com/company/MeTimesheet/controller/ImageController.java
@@ -1,7 +1,6 @@
 package com.company.MeTimesheet.controller;
 
 import com.company.MeTimesheet.entity.ImageEntity;
-import com.company.MeTimesheet.entity.ImageResponse;
 import com.company.MeTimesheet.service.ImageService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/images")
@@ -25,59 +23,11 @@ public class ImageController {
     }
 
 
-    private final String imageDirectory = "/Users/anaralizada/Desktop"; // Replace with the actual path
-
-    @GetMapping("/with-links")
-    public List<ImageResponse> getAllImagesWithLinks() {
-        List<ImageEntity> images = imageService.getAllImages();
-        return images.stream()
-                .map(this::mapToImageResponse)
-                .collect(Collectors.toList());
-    }
-
-    private ImageResponse mapToImageResponse(ImageEntity imageEntity) {
-        ImageResponse response = new ImageResponse();
-        response.setId(imageEntity.getImageId());
-        response.setImageName(imageEntity.getImageName());
-        response.setProductQuantity(imageEntity.getProductQuantity());
-        response.setImageUrl("/api/images/" + imageEntity.getImageId() + "/image");
-        return response;
-    }
-
-//    @GetMapping("/{id}/image")
-//    public ResponseEntity<Resource> getImage(@PathVariable Long id) {
-//        ImageEntity imageEntity = imageService.getImageById(id).orElse(null);
-//
-//        if (imageEntity != null) {
-//            Path imagePath = Paths.get(imageDirectory, id + ".jpg");
-//
-//            try {
-//                Resource resource = new UrlResource(imagePath.toUri());
-//                return ResponseEntity.ok()
-//                        .body(resource);
-//            } catch (IOException e) {
-//                e.printStackTrace(); // Handle exception appropriately
-//                return ResponseEntity.notFound().build();
-//            }
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-
-
     @CrossOrigin(origins = "http://localhost:5173/")
     @GetMapping
     public List<ImageEntity> getAllImages() {
         return imageService.getAllImages();
     }
-
-//    @GetMapping("/image-urls")
-//    public List<String> getImageUrls() {
-//        List<ImageEntity> images = imageService.getAllImages();
-//        return images.stream()
-//                .map(image -> "/api/images/" + image.getImageId() + "/image")
-//                .collect(Collectors.toList());
-//    }
 
 
     @CrossOrigin(origins = "http://localhost:5173/")
